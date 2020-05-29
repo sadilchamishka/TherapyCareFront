647d41c205e1e2ab63b7f2c877a265094639b9d0
sadilchamishka <sandilchamishka@gmail.com>
2020-05-29 09:04:13 +0530

    Updates

---
+++ precache-manifest.a8d479b59b345fbbc24831de123802e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31aa3a6e424ad823274237d56b6f018d",
+    "url": "/TherapyCareFront/index.html"
+  },
+  {
+    "revision": "559b7e645836d050704d",
+    "url": "/TherapyCareFront/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "80499e27316a01b1390b",
+    "url": "/TherapyCareFront/static/js/2.08ab5e21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TherapyCareFront/static/js/2.08ab5e21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "559b7e645836d050704d",
+    "url": "/TherapyCareFront/static/js/main.99edc712.chunk.js"
+  },
+  {
+    "revision": "00d3d0a6d92a0ba9d843",
+    "url": "/TherapyCareFront/static/js/runtime-main.3995fba8.js"
+  }
+]);
